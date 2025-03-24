db7b8b5202c86b82aa1e847ee43ca419e6e31a24
M.Syamsandi <zxvcklh@gmail.com>
2025-03-24 10:01:00 +0700

    git init and add docker-compose

---
+++ services/config-server/src/test/java/com/syamsandi/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syamsandi.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
